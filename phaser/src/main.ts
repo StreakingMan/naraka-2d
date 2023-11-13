04a39eb712853ffb98eb27be15b398f32036ba8c
streakingman <z_max_y@163.com>
2023-11-13 17:48:26 +0800

    chore: 更新phaser和spine插件版本，调整publicDir path

--- phaser/src/main.ts
+++ phaser/src/main.ts
@@ -1,73 +1,54 @@
 import * as Phaser from 'phaser';
+import {
+    Skin,
+    SkinsAndAnimationBoundsProvider,
+    SpineGameObject,
+    SpinePlugin,
+} from '@esotericsoftware/spine-phaser';
 import GameConfig = Phaser.Types.Core.GameConfig;
 import ScenePreloadCallback = Phaser.Types.Scenes.ScenePreloadCallback;
 import SceneCreateCallback = Phaser.Types.Scenes.SceneCreateCallback;
 import SceneUpdateCallback = Phaser.Types.Scenes.SceneUpdateCallback;
 import SpriteWithDynamicBody = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
-import StaticGroup = Phaser.Physics.Arcade.StaticGroup;
 
 let player: SpriteWithDynamicBody;
-let platforms: StaticGroup;
 let cursors: CursorKeys;
 const preload: ScenePreloadCallback = function () {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', {
-        frameWidth: 32,
-        frameHeight: 48,
-    });
-    // @ts-ignore
-    this.load.spine(
-        'spineboy',
-        'assets/spineboy-pro.json',
-        'assets/spineboy-pro.atlas',
-        true
+    this.load.spineBinary(
+        'mix-and-match-data',
+        'assets/mix-and-match-pro/mix-and-match-pro.skel'
+    );
+    this.load.spineAtlas(
+        'mix-and-match-atlas',
+        'assets/mix-and-match-pro/mix-and-match-pro.atlas'
     );
 };
 
+let mixAndMatch: SpineGameObject;
 const create: SceneCreateCallback = function () {
-    // 背景
-    this.add.image(400, 300, 'sky').setScale(1.2);
-    // 静态平台
-    platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-    // 玩家
-    player = this.physics.add.sprite(100, 450, 'dude');
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1,
-    });
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20,
-    });
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1,
-    });
-
-    // spine
-    const spineboy = this.add
-        // @ts-ignore
-        .spine(400, 500, 'spineboy', 'idle', true)
-        .setScale(0.2);
-
-    // 碰撞
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(spineboy, platforms);
+    mixAndMatch = this.add.spine(
+        400,
+        500,
+        'mix-and-match-data',
+        'mix-and-match-atlas',
+        new SkinsAndAnimationBoundsProvider(null, ['full-skins/girl'])
+    );
+    mixAndMatch.scale = 0.5;
+    mixAndMatch.animationState.setAnimation(0, 'walk', true);
+    const skeletonData = mixAndMatch.skeleton.data;
+    const skin = new Skin('custom');
+    skin.addSkin(skeletonData.findSkin('skin-base') as Skin);
+    skin.addSkin(skeletonData.findSkin('nose/short') as Skin);
+    skin.addSkin(skeletonData.findSkin('eyelids/girly') as Skin);
+    skin.addSkin(skeletonData.findSkin('eyes/violet') as Skin);
+    skin.addSkin(skeletonData.findSkin('hair/brown') as Skin);
+    skin.addSkin(skeletonData.findSkin('clothes/hoodie-orange') as Skin);
+    skin.addSkin(skeletonData.findSkin('legs/pants-jeans') as Skin);
+    skin.addSkin(skeletonData.findSkin('accessories/bag') as Skin);
+    skin.addSkin(skeletonData.findSkin('accessories/hat-red-yellow') as Skin);
+    mixAndMatch.skeleton.setSkin(skin);
+    mixAndMatch.skeleton.setToSetupPose();
 
     // 键盘
     if (this.input.keyboard) cursors = this.input.keyboard.createCursorKeys();
@@ -77,50 +58,44 @@ const update: SceneUpdateCallback = function () {
     if (!cursors) {
         return;
     }
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-        player.anims.play('left', true);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-        player.anims.play('right', true);
-    } else {
-        player.setVelocityX(0);
-        player.anims.play('turn');
+    if (cursors.space.isDown) {
+        console.log('space');
+        mixAndMatch.animationState.setAnimation(0, 'dress-up', false);
+        mixAndMatch.animationState.addAnimation(0, 'walk', true, 0);
     }
-
-    if (cursors.up.isDown /*&& player.body.touching.down*/) {
-        player.setVelocityY(-630);
+    if (cursors.down.isDown) {
+        console.log('down');
+        mixAndMatch.animationState.setAnimation(0, 'dance', false);
+        mixAndMatch.animationState.addAnimation(0, 'walk', true, 0);
+    }
+    if (cursors.left.isDown) {
+        console.log('left');
+        mixAndMatch.animationState.setAnimation(0, 'walk', true);
+        mixAndMatch.scaleX = -0.5;
+    }
+    if (cursors.right.isDown) {
+        console.log('right');
+        mixAndMatch.animationState.setAnimation(0, 'walk', true);
+        mixAndMatch.scaleX = 0.5;
     }
 };
 
 const config: GameConfig = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     width: 800,
     height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 3000 },
-            debug: false,
-        },
-    },
     scene: {
         preload,
         create,
         update,
-        pack: {
-            files: [
-                {
-                    type: 'scenePlugin',
-                    key: 'SpinePlugin',
-                    url: 'local-plugins/SpinePluginDebug.js',
-                    sceneKey: 'spine',
-                },
-            ],
-        },
+    },
+    plugins: {
+        scene: [
+            { key: 'spine.SpinePlugin', plugin: SpinePlugin, mapping: 'spine' },
+        ],
     },
 };
 
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
 
 export {};
