5940b18b377a677be4c1b896abe20680e207e637
streakingman <z_max_y@163.com>
2023-11-22 17:05:43 +0800

    chore: pixi spine demo

--- pixi/src/demo.tsx
+++ pixi/src/demo.tsx
@@ -1,6 +1,7 @@
-import { BlurFilter } from 'pixi.js';
+import { Application, Assets, BlurFilter } from 'pixi.js';
 import { Stage, Container, Sprite, useTick } from '@pixi/react';
 import { useMemo, useReducer, useRef } from 'react';
+import { Spine } from 'pixi-spine';
 
 const reducer = (_: any, { data }: any) => data;
 const Bunny = () => {
@@ -42,3 +43,31 @@ export const Demo = () => {
         </Stage>
     );
 };
+
+const app = new Application();
+// @ts-ignore
+document.body.appendChild(app.view);
+
+Assets.load('/assets/mix-and-match-pro/mix-and-match-pro.skel').then(
+    (resource) => {
+        console.log(resource);
+        const animation = new Spine(resource.spineData);
+
+        animation.skeleton.setSkinByName('full-skins/girl');
+
+        animation.x = app.screen.width / 2;
+        animation.y = app.screen.height;
+
+        // add the animation to the scene and render...
+        app.stage.addChild(animation);
+
+        if (animation.state.hasAnimation('idle')) {
+            // run forever, little boy!
+            animation.state.setAnimation(0, 'idle', true);
+            // dont run too fast
+            animation.state.timeScale = 0.1;
+            // update yourself
+            animation.autoUpdate = true;
+        }
+    }
+);
--- pixi/src/pixi-spine-4.1.ts
+++
@@ -1,5 +0,0 @@
-import '@pixi-spine/loader-base'; // Side effect install the loader
-// @ts-ignore
-export * from '@pixi-spine/runtime-4.1';
-// @ts-ignore
-export * from '@pixi-spine/base';
